b54a94d7f01a280a5149b18860e04c7144e1b026
pandacommit <hanzhang.tw@gmail.com>
2024-03-08 01:29:10 +0900

    create orders table

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -54,3 +52,7 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 }
+
+
+
+
